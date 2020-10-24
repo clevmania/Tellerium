82861ee4c40a24a1f04980c47ab1f226c57784d9
clevmania <ernestcleverest@yahoo.com>
2020-10-24 15:43:32 +0100

    feat: wire up the login logic

--- app/src/main/java/com/clevmania/tellerium/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/clevmania/tellerium/ui/auth/LoginFragment.kt
@@ -1,20 +1,20 @@
 package com.clevmania.tellerium.ui.auth
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.clevmania.tellerium.R
+import com.clevmania.tellerium.ui.base.BaseFragment
+import com.clevmania.tellerium.utils.EventObserver
+import com.clevmania.tellerium.utils.ValidationException
+import com.clevmania.tellerium.utils.ValidationType
+import com.clevmania.tellerium.utils.validate
+import kotlinx.android.synthetic.main.login_fragment.*
 
-class LoginFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
-    private lateinit var viewModel: LoginViewModel
+class LoginFragment : BaseFragment() {
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +23,50 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.login_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mbSignIn.setOnClickListener { signUpUser() }
+        tvSignUp.setOnClickListener {
+            // nnavigate to signnup
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        with(viewModel) {
+            progress.observe(viewLifecycleOwner, EventObserver {
+                toggleBlockingProgress(it)
+            })
+
+            authRequest.observe(viewLifecycleOwner, EventObserver {
+                when (it) {
+                    is AuthSuccess -> {
+                        // navigate to farmer screen
+                    }
+                    is AuthError -> {
+                        showErrorDialog(it.error)
+                    }
+                    is InvalidUser -> {
+                        showErrorDialog(getString(R.string.wrong_credential))
+                    }
+                }
+            })
+        }
+    }
+
+    private fun signUpUser() {
+        try {
+            val email = tilEmail.validate(ValidationType.EMAIL, getString(R.string.email))
+            val password = tilPassword.validate(
+                ValidationType.PASSWORD, getString(R.string.password)
+            )
+            viewModel.signInWithEmail(email, password)
+
+        } catch (ex: ValidationException) {
+            ex.printStackTrace()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 
 }
