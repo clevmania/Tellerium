4325712998b27200a702a0b9d2a062df5d12b932
clevmania <ernestcleverest@yahoo.com>
2020-10-24 14:07:32 +0100

    feat: added splash

--- app/src/main/java/com/clevmania/tellerium/MainActivity.kt
+++ app/src/main/java/com/clevmania/tellerium/MainActivity.kt
@@ -3,10 +3,13 @@ package com.clevmania.tellerium
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     private val navController by lazy { findNavController(R.id.nav_host_fragment)}
 
@@ -15,11 +18,18 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        setupActionBarWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.splashFragment
+            ), null
+        )
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/clevmania/tellerium/ui/splash/SplashFragment.kt
@@ -0,0 +1,14 @@
+package com.clevmania.tellerium.ui.splash
+
+import androidx.fragment.app.Fragment
+
+/**
+ * @author by Lawrence on 10/24/20.
+ * for Tellerium
+ */
+class SplashFragment : Fragment() {
+    override fun onStart() {
+        super.onStart()
+
+    }
+}
