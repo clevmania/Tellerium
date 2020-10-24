16c2cea2ce2c93a4376e399007f2d78aab97cac7
clevmania <ernestcleverest@yahoo.com>
2020-10-24 13:50:16 +0100

    feat: architect the single activity

--- app/src/main/java/com/clevmania/tellerium/MainActivity.kt
+++ app/src/main/java/com/clevmania/tellerium/MainActivity.kt
@@ -2,10 +2,24 @@ package com.clevmania.tellerium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment)}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
