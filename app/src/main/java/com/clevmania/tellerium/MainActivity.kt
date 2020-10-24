0e32671eead0a8fadcadd4e3488f3b13221b1eb0
clevmania <ernestcleverest@yahoo.com>
2020-10-24 21:55:39 +0100

    feat: manage showing of toolbar on fragments

--- app/src/main/java/com/clevmania/tellerium/MainActivity.kt
+++ app/src/main/java/com/clevmania/tellerium/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.clevmania.tellerium.utils.makeGone
+import com.clevmania.tellerium.utils.makeVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,9 +29,24 @@ class MainActivity : AppCompatActivity() {
         )
 
         setupActionBarWithNavController(navController,appBarConfiguration)
+        toolbarVisibilityManager()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun toolbarVisibilityManager() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.loginFragment, R.id.registerFragment,
+                R.id.splashFragment -> {
+                    toolbar.makeGone()
+                }
+                else -> {
+                    toolbar.makeVisible()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/clevmania/tellerium/utils/ViewExt.kt
@@ -0,0 +1,13 @@
+package com.clevmania.tellerium.utils
+
+import android.view.View
+
+/**
+ * @author by Lawrence on 10/24/20.
+ * for Tellerium
+ */
+fun View.makeVisible(){ this.visibility = View.VISIBLE }
+
+fun View.makeGone(){ this.visibility = View.GONE }
+
+fun View.makeInVisible() { this.visibility = View.INVISIBLE}
