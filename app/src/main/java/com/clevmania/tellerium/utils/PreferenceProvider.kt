7e6a211fa2fd96e4b353016165004e815a856a03
clevmania <ernestcleverest@yahoo.com>
2020-10-28 10:49:34 +0100

    tell dagger how to provide preference provider

--- app/src/main/java/com/clevmania/tellerium/utils/PreferenceProvider.kt
+++ app/src/main/java/com/clevmania/tellerium/utils/PreferenceProvider.kt
@@ -4,12 +4,13 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.clevmania.tellerium.utils.Constants.PREF_DEFAULT_IMAGE_URL
 import com.clevmania.tellerium.utils.Constants.PREF_IMAGE_URL_KEY
+import javax.inject.Inject
 
 /**
  * @author by Lawrence on 10/27/20.
  * for Tellerium
  */
-class PreferenceProvider(private val sharedPref: SharedPreferences) {
+class PreferenceProvider @Inject constructor(private val sharedPref: SharedPreferences) {
 
     fun setImageBaseUrl(url: String) {
         sharedPref.edit(commit = true) { putString(PREF_IMAGE_URL_KEY, url) }
